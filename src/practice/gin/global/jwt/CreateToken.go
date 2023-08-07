2dd55354a336d877119d8e21ab95a358ad70423c
jeayoung <k01066624566@gmail.com>
2023-08-07 16:06:18 +0900

    토큰마다 만료 기간 추가

--- src/practice/gin/global/jwt/CreateToken.go
+++ src/practice/gin/global/jwt/CreateToken.go
@@ -11,20 +11,20 @@ var refreshType = "REFRESH"
 
 func CreateAccessToken(userEmail string) (string, error) {
 	accessSecret := os.Getenv("ACCESS_SECRET")
-	return createToken(userEmail, accessSecret, accessType)
+	return createToken(userEmail, accessSecret, accessType, 15)
 }
 
 func CreateRefreshToken(userEmail string) (string, error) {
 	refreshSecret := os.Getenv("REFRESH_SECRET")
-	return createToken(userEmail, refreshSecret, accessType)
+	return createToken(userEmail, refreshSecret, accessType, 60*24)
 }
 
-func createToken(userEmail string, secret string, tokenType string) (string, error) {
+func createToken(userEmail string, secret string, tokenType string, ttl int) (string, error) {
 	atClaims := jwt.MapClaims{}
 	atClaims["authorized"] = true
 	atClaims["user_email"] = userEmail
 	atClaims["type"] = tokenType
-	atClaims["exp"] = time.Now().Add(time.Minute * 15).Unix()
+	atClaims["exp"] = time.Now().Add(time.Duration(ttl) * time.Minute).Unix()
 	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
 	token, err := at.SignedString([]byte(secret))
 	if err != nil {
