5c9d37ccdf6f17ff89c2b75e873c97d947b4fb22
jeayoung <k01066624566@gmail.com>
2023-08-09 16:48:46 +0900

    add refreshToken Save

--- src/practice/gin/domain/auth/service/SignInService.go
+++ src/practice/gin/domain/auth/service/SignInService.go
@@ -9,6 +9,9 @@ import (
 	"gin_practice/src/practice/gin/global/jwt"
 	jwtException "gin_practice/src/practice/gin/global/jwt/exception"
 	"gin_practice/src/practice/gin/global/passwordEncoder"
+	"gin_practice/src/practice/gin/global/redis"
+	"os"
+	"strconv"
 )
 
 func SignIn(request request.SignInRequest) (response.SignInResponse, error) {
@@ -21,6 +24,8 @@ func SignIn(request request.SignInRequest) (response.SignInResponse, error) {
 	}
 	accessToken, accessErr := jwt.CreateAccessToken(user.Email)
 	refreshToken, refreshErr := jwt.CreateRefreshToken(user.Email)
+	refreshExp, _ := strconv.Atoi(os.Getenv("REFRESH_EXP"))
+	redis.SaveValue("RefreshToken", refreshToken, user.Email, refreshExp)
 	if accessErr != nil || refreshErr != nil {
 		return response.SignInResponse{}, jwtException.JwtGenerateException()
 	}
