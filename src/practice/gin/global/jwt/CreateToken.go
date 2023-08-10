7db82bf24e63a796a256fa6ae7c28790c3ab0d8a
jeayoung <k01066624566@gmail.com>
2023-08-10 14:49:16 +0900

    add getTokenExp method

--- src/practice/gin/global/jwt/CreateToken.go
+++ src/practice/gin/global/jwt/CreateToken.go
@@ -35,3 +35,13 @@ func createToken(userEmail string, secret string, tokenType string, ttl int) (st
 	}
 	return token, nil
 }
+
+func getAccessExp() time.Duration {
+	exp, _ := strconv.Atoi(os.Getenv("ACCESS_EXP"))
+	return time.Duration(exp) * time.Minute
+}
+
+func getRefreshExp() time.Duration {
+	exp, _ := strconv.Atoi(os.Getenv("REFRESH_EXP"))
+	return time.Duration(exp) * time.Minute
+}
