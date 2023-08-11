6cd9abe93c5cda0d0fe7c755ceeaaee3547cfb3a
jeayoung <k01066624566@gmail.com>
2023-08-11 15:06:47 +0900

    add middleware

---
+++ src/practice/gin/global/security/filter/JwtReqFilter.go
@@ -0,0 +1,23 @@
+package filter
+
+import (
+	"errors"
+	"gin_practice/src/practice/gin/global/exception"
+	"gin_practice/src/practice/gin/global/jwt"
+	"gin_practice/src/practice/gin/global/security"
+	"github.com/gin-gonic/gin"
+)
+
+func JwtReqFilterGroup(g *gin.RouterGroup) {
+	g.Use(func(c *gin.Context) {
+		user, err := jwt.GetCurrentUser(c.Request)
+		if err != nil {
+			var globalException exception.GlobalException
+			errors.As(err, &globalException)
+			response := globalException.ToErrorResponse()
+			c.JSON(globalException.Code, response)
+			c.Abort()
+		}
+		security.SetUser(*user)
+	})
+}
