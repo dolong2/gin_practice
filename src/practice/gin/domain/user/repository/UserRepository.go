6bac31a362fb439b4393bbf163762b389a5001e2
jeayoung <k01066624566@gmail.com>
2023-08-17 08:43:18 +0900

    delete log in UserRepository

--- src/practice/gin/domain/user/repository/UserRepository.go
+++ src/practice/gin/domain/user/repository/UserRepository.go
@@ -2,13 +2,11 @@ package repository
 
 import (
 	"gin_practice/src/practice/gin/domain/user/entity"
-	"log"
 )
 
 var repo = make(map[string]entity.User)
 
 func Save(user entity.User) {
-	log.Println(user.Email)
 	repo[user.Email] = user
 }
 
